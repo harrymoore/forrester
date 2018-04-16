51e0843661dc93f376f702b4e5f8fdd59af1a0bc
Harry Moore <harry.moore@cloudcms.com>
2018-04-16 17:53:35 -0400

    load new gadgets in module

--- modules/ui/index.js
+++ modules/ui/index.js
@@ -1,3 +1,5 @@
 define(function(require) {
     require("./gadgets/forrester-ui/account-list.js");
+    require("./gadgets/albums/albums-list.js");
+    require("./gadgets/artists/artists-list.js");
 });
